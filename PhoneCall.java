453bd1c662806865e8147ee43f23f9c6d09e7207
Abenezer Y Bekele <113382918+AbeBekele12@users.noreply.github.com>
2024-02-05 17:51:56 -0600

    Update PhoneCall.java

--- PhoneCall.java
+++ PhoneCall.java
@@ -1,10 +1,4 @@
-/**
- * 
- */
-/**
- * @author abiye
- *
- */
+
 package wq;
 
 public abstract class PhoneCall   implements InterfacePhone { 
@@ -19,23 +13,12 @@ public abstract void Thing();
 	public PhoneCall( String Phone ) {
 		this.PhoneNumber= Phone;
 	}
-	//copy constructor
-//	public PhoneCall(PhoneCall obj) {
-//		this.PhoneNumber=obj.PhoneNumber;	}
-//	//MakeCopy
-//	public void Makecopy(PhoneCall obj) {
-//		this.PhoneNumber=obj.PhoneNumber;	}
-	//Set Methods
+	
 	public void setPhoneNumber(String Phone) {
 		this.PhoneNumber=Phone ; 
 		}
-	//get Methods
+	
 	public String getPhone_Number() {
 		return PhoneNumber;		}
-	
-	// ToString	
-//	public String toString() {
-//		return ( "Phone_Number :"+PhoneNumber+ "The price of call is :"+Price_The_Call);
-//	}
-//	
+		
 }
